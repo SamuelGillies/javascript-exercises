fd6425215a8640bb9b608020285056ad128ff4d0
Sam Gillies <sam@samgillies.com>
2022-09-29 16:03:02 +0100

    reverseString function complete, tests return correct result

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // put string in array 
+    let text = string;
+    let array = text.split("");
 
+    // reverse array
+    array.reverse(); 
+
+    // turn array into string 
+    let stringReversed = array.join(""); 
+
+    // return reversed string
+    return stringReversed 
 };
 
 // Do not edit below this line
