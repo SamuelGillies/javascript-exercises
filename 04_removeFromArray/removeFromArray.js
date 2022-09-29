608307274af232f04b6143111e3ddbedaa3e6886
Sam Gillies <sam@samgillies.com>
2022-09-29 16:56:46 +0100

    removeFromArray structure in place, added method for separating function arguments into chunks

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    
+    let inputs = [...args]; 
+
+    // place array in one var 
+    let array = inputs.slice(0,1); 
+
+    // place all other arguments in another var
+    let filter = inputs.slice(1); 
+
+    // create include arrays for storage of filtered values  
+    let include = []; 
+
+    // test each item of array against each item of arguments
+
+    for (let item of array) {               // iterate over array 
+        for (let argument of filter) {   // iterate over arguments
+            if (item != argument) {         // test if array value matches argument value
+                include.push(item);         // if no, add to include array
+            }
+        }
+    }
+
+    // return final list of included items
+    return include;
 
 };
 
