9865b4e3080e8524e9cd3a849ffe2ebbdd126a07
Sam Gillies <sam@samgillies.com>
2022-10-04 13:35:16 +0100

    ftoc and ctof functions written correctly, all tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
+const ftoc = function(degree) {
+
+  // (0°F − 32) × 5/9 = -17.78°C
+let celcius = (degree - 32) * (5/9);
+return Math.round(celcius * 10) / 10; 
 
 };
 
-const ctof = function() {
+const ctof = function(degree) {
 
+//  (0°C × 9/5) + 32 = 32°F
+let farenheight = (degree * (9/5)) + 32; 
+return Math.round(farenheight * 10) / 10; 
 };
 
 // Do not edit below this line
