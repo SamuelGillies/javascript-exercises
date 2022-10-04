215c454693826732544ec578674727653941ce09
Sam Gillies <sam@samgillies.com>
2022-10-04 13:15:08 +0100

    leapYears function written, passes all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if ((year % 4) == 0) {          // test if year is divisible by 4. if yes proceed to next test, if not then false
+        if ((year % 100) == 0) {      // test if year is divisible by 100. if yes proceed to next test, if not then true
+            if (year % 400 == 0) {  // test if year is divisible by 400. if yes then true, if not then false
+                return true; 
+            } else {
+                return false; 
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false; 
+    }
 };
 
 // Do not edit below this line
