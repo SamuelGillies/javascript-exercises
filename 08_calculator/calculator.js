2a5a39c496e327c1910dc4572e9e7abf11f6d779
Sam Gillies <sam@samgillies.com>
2022-11-03 16:31:46 +0000

    calculator exercise completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b; 
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b; 
 };
 
-const sum = function() {
-	
+const sum = function([...array]) {
+	return array.reduce((a, b) => a + b, 0); 
 };
 
-const multiply = function() {
-
+const multiply = function([...array]) {
+  return array.reduce((a, b) => a * b, 1); 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b); 
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+
+  let result = a; 
+  if (a === 0 || a === 1) {return 1}; 
+
+  while (a > 1) {
+    a--; 
+    result = result * a; 
+  }
+
+  return result; 
+
 };
 
 // Do not edit below this line
