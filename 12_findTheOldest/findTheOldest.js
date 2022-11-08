4cbbe1af91d4f826287505e510240086a2246d3b
Sam Gillies <sam@samgillies.com>
2022-11-08 17:05:32 +0000

    check one completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+    const checked = array.map(item => item); 
+        // if (item.hasOwnProperty('yearOfDeath') == false) {
+        //     item.yearOfDeath = (new Date()).getFullYear();
+        // } else {
+        //     item = item; 
+        // }
+    
+
+    console.table(checked); 
+
+
+    const ordered = checked.sort(function(a, b) {
+        return  ((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)) ? -1 
+                : ((a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth)) ? 1
+                : 0; 
+    });
+    return ordered[0];
 };
 
 // Do not edit below this line
